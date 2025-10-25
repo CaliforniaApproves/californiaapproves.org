bfeb54e2d1f9690189d9cba7d23daadb80135443
Alan Savage <3028205+asavageiv@users.noreply.github.com>
2025-10-25 13:59:38 -0700

    Reword header and tagline

--- src/pages/Home/banner.tsx
+++ src/pages/Home/banner.tsx
@@ -34,13 +34,13 @@ const Banner = () => {
             <div className='m-auto px-8 pb-14 lg:pb-0'>
                 <img src={strongArm} alt="strong arm" width="338" height="316" className='aspect-169/158'/>
                 <h1 className='text-green banner-drop-shadow'>
-                    The <span className='text-purple'>Easiest</span> Way To Make <br className='hidden lg:inline'/>
-                    The <span className='text-purple'>Biggest</span> Impact
+                    <span class="text-orange">Approval Voting</span> <br class="hidden lg:inline"></br>
+                    <span class="text-purple">Unites Us</span>
                 </h1>
                 <div className='static max-w-[500px] mt-9'>
                     <Arrow className='absolute'/>
                     <p className='text-large-bold flex flex-row indent-24 font-bold lg:font-normal'>
-                        Approval Voting gives voters the power to support multiple candidates equally so that stronger representatives with broad support are always elected.
+                        Approval Voting unites us by electing the candidates we most agree on instead of divisive ones. Better representatives means stronger communities, less corruption, and better government.
                     </p>
                 </div>
             </div>
