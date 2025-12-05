5bff56b502968a578f551ca187a30787d582add1
Alan Savage <3028205+asavageiv@users.noreply.github.com>
2025-12-05 01:27:01 +0000

    Update sub-tagline text

--- src/pages/Home/banner.tsx
+++ src/pages/Home/banner.tsx
@@ -34,13 +34,13 @@ const Banner = () => {
             <div className='m-auto px-8 pb-14 lg:pb-0'>
                 <img src={strongArm} alt="strong arm" width="338" height="316" className='aspect-169/158'/>
                 <h1 className='text-green banner-drop-shadow'>
-                    <span class="text-orange">Approval Voting</span> <br class="hidden lg:inline"></br>
-                    <span class="text-purple">Unites Us</span>
+                    Approval Voting <br class="hidden lg:inline"></br>
+                    Unites Us
                 </h1>
                 <div className='static max-w-[500px] mt-9'>
                     <Arrow className='absolute'/>
                     <p className='text-large-bold flex flex-row indent-24 font-bold lg:font-normal'>
-                        Approval Voting unites us by electing the candidates we most agree on instead of divisive ones. Better representatives means stronger communities, less corruption, and better government.
+                        Politics doesn't have to be so polarizing. Approval Voting changes the game so we can have representatives that focus on what we agree on instead of what divides us.
                     </p>
                 </div>
             </div>
