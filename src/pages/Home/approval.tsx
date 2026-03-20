51345df8f3dc0c75496da1564bf94f61a6160cc8
Alan Savage <3028205+asavageiv@users.noreply.github.com>
2026-03-20 00:40:02 +0000

    Fix padding in AboutApproval on mobile

--- src/pages/Home/approval.tsx
+++ src/pages/Home/approval.tsx
@@ -21,7 +21,7 @@ import CheckMark from '../../assets/icons/Yellow-Check.svg?react';
 // create a component
 const AboutApproval = () => {
     return (
-        <div className='bg-purple flex flex-col-reverse lg:flex-row justify-around text-white pt-28'>
+        <div className='bg-purple flex flex-col-reverse lg:flex-row justify-around text-white pt-12 lg:pt-28'>
             <img className="w-fit h-fit" src={Ballot}/>
             <div className='flex flex-col max-w-full gap-y-5 lg:max-w-[50%] px-6 lg:px-0 font-bold lg:font-normal pb-28'>
                 <h1 className='text-white'>
